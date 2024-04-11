5346e96141538240d0661c7c46f69bb8888963c8
Dinesh kumar <72866547+Deathslayer89@users.noreply.github.com>
2024-04-11 20:54:01 +0530

    revert back to only cards

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
       const params = {
         submit_type: 'pay',
         mode: 'payment',
-        payment_method_types: ['card','wallets'],
+        payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
           { shipping_rate: 'shr_1M4hXbSF79z78Oxzli6Wcnpg' },
